7fffc5a399769edc617e1e38d1273d98e5129354
vilayat571 <vilayat571@.com>
2022-11-30 15:25:25 -0700

    the last changes in the main for two section

--- src/parts/Main/Brief.tsx
+++ src/parts/Main/Brief.tsx
@@ -3,7 +3,8 @@ import Question from "../../atoms/Main/Brief/Question";
 
 export default function Brief() {
   return (
-    <div className="w-full grid lg:grid-cols-3 xl:grid-cols-3 md:grid-cols-2 sm:grid-cols-2 h-auto  py-48">
+    <div className="w-full grid lg:grid-cols-3 xl:grid-cols-3 md:grid-cols-2 sm:grid-cols-2 h-auto  
+   xl:py-48 sm:py-28 md:py-28 lg:py-48">
       <Question />
       <Answer />
     </div>
