ec005e850fcede252c2eb1dfb4113226ed148052
vilayat571 <vilayat571@.com>
2022-11-30 15:21:32 -0700

    Some changes

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,7 +1,6 @@
 import { ReactNode } from "react";
 import Navbar from "../components/Navbar/Navbar";
 
-
 interface ILayout {
   children: ReactNode;
 }
@@ -10,7 +9,7 @@ export default function Layout(props: ILayout) {
   return (
     <div className=" sm:py-10 md:py-10 lg:py-16 xl:py-16 flex flex-col items-center">
       <Navbar />
-      <div className=" w-4/5 pt-36 flex justify-center items-center">
+      <div className=" sm:w-11/12 md:w-11/12 lg:w-4/5 xl:w-4/5 sm:pt-24 md:pt-24 lg:pt-36 xl:pt-36 flex justify-center items-center">
         {props.children}
       </div>
     </div>
