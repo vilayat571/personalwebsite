4a584b01967b7bff3850588ce8a239a0c7e510e0
vilayat571 <vilayat571@.com>
2022-11-30 15:18:04 -0700

    Some changes

--- src/components/Main/Headercontent.tsx
+++ src/components/Main/Headercontent.tsx
@@ -5,16 +5,13 @@ import Tolink from "../../atoms/Main/Tolink";
 export default function Headercontent() {
   return (
     <div
-      className="col-span-1  flex flex-col 
-   md:items-center sm:items-center 
-    lg:items-start xl:items-start"
-    >
+      className="col-span-1 flex flex-col md:items-center sm:items-center lg:items-start xl:items-start" >
       <Pelement
         text={
           "Helping people make the world a better place through quality software."
         }
         stil={
-          "text-white sm:w-full md:w-full lg:w-4/5 xl:w-4/5 text-[36px] tracking-[0.6px] "
+          "text-white sm:w-full md:w-full lg:w-4/5 xl:w-4/5 sm:text-3xl md:text-3xl xl:text-36px lg:text-[36px] tracking-[0.6px] "
         }
       />
       <Rlink />
