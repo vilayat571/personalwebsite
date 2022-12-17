8da005ea076a84c4c2bc033fca71906889937989
vilayat571 <vilayat571@.com>
2022-12-17 16:30:11 -0700

    some changes

--- src/atoms/Navbar/TokenNav.tsx
+++ src/atoms/Navbar/TokenNav.tsx
@@ -8,11 +8,9 @@ export default function TokenNav() {
 
   useEffect(() => {
     const details = localStorage.getItem("userDetails");
-    details && setName(details ? JSON.parse(details).user_details.username :'');
+    details ? setName(details ? JSON.parse(details).user_details.username : "") : '';
     setToken(localStorage.getItem("jwt"));
   }, []);
-
-
 
   return (
     <>
