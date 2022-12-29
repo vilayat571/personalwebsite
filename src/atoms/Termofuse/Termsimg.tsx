84277684cde8145c1ef87bdb4004e18089ee4246
vilayat571 <vilayat571@.com>
2022-12-28 19:40:30 -0700

    some changes

--- src/atoms/Termofuse/Termsimg.tsx
+++ src/atoms/Termofuse/Termsimg.tsx
@@ -1,9 +1,10 @@
+import desktop from "../../assets/images/desktop.webp";
 export default function Termsimg() {
   return (
     <img
       className="xl:h-[600px] lg:h-[600px] md:h-auto sm:h-auto object-fill mt-2 
       xl:mb-28 lg:mb-28 md:mb-20 sm:mb-12 rounded-md border-0"
-      src="https://images.unsplash.com/photo-1487017159836-4e23ece2e4cf?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YWVzdGhldGljJTIwd29ya3xlbnwwfHwwfHw%3D&w=1000&q=80"
+      src={desktop}
       alt="terms of use"
     />
   );
