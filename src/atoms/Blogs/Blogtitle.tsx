35e2280308b8c1dc81e4968340e640deb3765b8a
vilayat571 <vilayat571@.com>
2023-01-07 20:31:42 -0700

    some changes

--- src/atoms/Blogs/Blogtitle.tsx
+++ src/atoms/Blogs/Blogtitle.tsx
@@ -1,6 +1,6 @@
 export default function Blogtitle(props: any) {
   return (
-    <div className="text-[#fff] text-xl mt-6 flex justify-between">
+    <div className="text-[#fff] text-xl mt-3 flex justify-between">
       <span className="line-clamp-1">{props.title}</span>
     </div>
   );
